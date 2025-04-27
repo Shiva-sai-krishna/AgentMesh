93fcee972282107321f0ce6359d785ef904f4924
Shiva-sai-krishna  <shivakrishnaanand@gmail.com>
2025-04-27 09:58:36 +0530

    Made Changes -

---
+++ client.py
@@ -0,0 +1,111 @@
+import os
+import socket
+import yaml
+import paramiko
+import threading
+import sys
+
+def load_config(path="config.yaml"):
+    with open(path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def ssh_run_command(ip, username, password, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=ip, username=username, password=password)
+
+    stdin, stdout, stderr = client.exec_command(command)
+    for line in stdout:
+        print(f"[{ip}] {line.strip()}")
+    for line in stderr:
+        print(f"[{ip} ERROR] {line.strip()}")
+    
+    client.close()
+
+def setup_worker(device_info, master_addr, master_port, world_size, rank):
+    ip = device_info["ip"]
+    username = device_info["username"]
+    password = device_info["password"]
+    work_dir = device_info["work_dir"]
+    python_env = device_info["python_env"]
+    hf_cache = device_info["hf_cache"]
+
+    command = f"""
+    cd {work_dir} && \
+    export MASTER_ADDR={master_addr} && \
+    export MASTER_PORT={master_port} && \
+    export WORLD_SIZE={world_size} && \
+    export RANK={rank} && \
+    source {python_env} && \
+    python3 worker.py
+    """
+    ssh_run_command(ip, username, password, command)
+
+def setup_master(device_info, master_addr, master_port, world_size, rank):
+    ip = device_info["ip"]
+    username = device_info["username"]
+    password = device_info["password"]
+    work_dir = device_info["work_dir"]
+    python_env = device_info["python_env"]
+
+    # Check if we are already on master
+    local_ip = socket.gethostbyname(socket.gethostname())
+
+    if local_ip == master_addr :
+        # Run locally
+        print("[Client] Running master.py locally.")
+        os.chdir(work_dir)
+        os.environ["MASTER_ADDR"] = master_addr
+        os.environ["MASTER_PORT"] = str(master_port)
+        os.environ["WORLD_SIZE"] = str(world_size)
+        os.environ["RANK"] = str(rank)
+
+        os.system(f"{python_env} master.py")
+    else:
+        # SSH into master node
+        print("[Client] SSH into master to run master.py")
+        command = f"""
+        cd {work_dir} && \
+        export MASTER_ADDR={master_addr} && \
+        export MASTER_PORT={master_port} && \
+        export WORLD_SIZE={world_size} && \
+        export RANK={rank} && \
+        {python_env} master.py
+        """
+        ssh_run_command(ip, username, password, command)
+
+def main():
+    config = load_config()
+
+    master_addr = config["master"]["ip"]
+    master_port = config["master"]["port"]
+    devices = config["workers"]
+
+    world_size = len(devices) + 1  # workers + master
+    threads = []
+
+    # Setup workers
+    for rank, device in enumerate(devices, start=1):
+        thread = threading.Thread(target=setup_worker, args=(device, master_addr, master_port, world_size, rank))
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all worker nodes to be ready
+    for thread in threads:
+        thread.join()
+
+    # Now setup master (rank 0)
+    master_device_info = {
+        "ip": master_addr,
+        "username": config["master"]["username"],
+        "password": config["master"]["password"],
+        "work_dir": config["master"]["work_dir"],
+        "python_env": config["master"]["python_env"],
+        "hf_cache": config["master"]["hf_cache"]
+    }
+
+    setup_master(master_device_info, master_addr, master_port, world_size, rank=0)
+
+if __name__ == "__main__":
+    main()
